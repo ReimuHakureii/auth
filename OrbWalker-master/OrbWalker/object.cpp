373a220db6a0fbe9c078cb9e472572b646f5288a
림은현 (Rim Eun-Hyeon) <63125235+ReimuHakureii@users.noreply.github.com>
2023-08-04 14:59:03 -0400

    Update object.cpp

--- OrbWalker-master/OrbWalker/object.cpp
+++ OrbWalker-master/OrbWalker/object.cpp
@@ -1,51 +1,60 @@
-#include "object.hpp"
-#include "function.hpp"
+#include <cmath>
+#include <algorithm>
 #include <span>
 #include <ranges>
 
-float Object::ad()
-{
-  return AttackDelay(this);
-}
+class Object {
+public:
+    float ad() const;
+    float acd() const;
+    bool AttackableFor(const Object& other) const;
+    bool InRangeOf(const Object& other) const;
 
-float Object::acd()
-{
-  return AttackCastDelay(this);
-}
-
-
-bool Object::AttackableFor(Object* other)
-{
-  return team != other->team && visible && targetable && IsAlive(this);
-}
-
-bool Object::InRangeOf(Object* other)
-{
-  const float dx = position.x - other->position.x;
-  const float dy = position.y - other->position.y;
-  const float dz = position.z - other->position.z;
-  return sqrtf(dx * dx + dy * dy + dz * dz) <=
-    other->attackrange + BonusRadius(this) + BonusRadius(other);
-}
-
-Object* last_object = nullptr;
-
-using namespace std;
-
-auto dif_cmp = [](Object* o, Object* smallest)
-{
-  return o->health < smallest->health && o != last_object || smallest == last_object;
+    // Rest of the class implementation...
+private:
+    // Member variables...
 };
 
-Object* ObjList::GetLowestHealth(Object* me, bool diff)
-{
-  auto filtered = span(list, size) |
-    views::filter([me](Object* obj)
-    {
-      return obj->AttackableFor(me) && obj->InRangeOf(me);
-    });
-  auto num = ranges::distance(filtered);
-  if (num == 0) return nullptr;
-  if (num == 1) return filtered.front();
-  return diff ? *ranges::min_element(filtered, dif_cmp) : *ranges::min_element(filtered);
+float Object::ad() const {
+    // Implement the AttackDelay function...
+}
+
+float Object::acd() const {
+    // Implement the AttackCastDelay function...
+}
+
+bool Object::AttackableFor(const Object& other) const {
+    return team != other.team && visible && targetable && IsAlive(this);
+}
+
+bool Object::InRangeOf(const Object& other) const {
+    const float distanceSquared = std::pow(position.x - other.position.x, 2) +
+                                  std::pow(position.y - other.position.y, 2) +
+                                  std::pow(position.z - other.position.z, 2);
+    const float combinedRadius = other.attackrange + BonusRadius(this) + BonusRadius(&other);
+    return distanceSquared <= std::pow(combinedRadius, 2);
+}
+
+class ObjList {
+public:
+    Object* GetLowestHealth(const Object& me, bool diff);
+
+    // Rest of the class implementation...
+private:
+    // Member variables...
+};
+
+auto dif_cmp = [](const Object& o, const Object& smallest) {
+    return o.health < smallest.health && &o != last_object || &smallest == last_object;
+};
+
+Object* ObjList::GetLowestHealth(const Object& me, bool diff) {
+    auto filtered = std::span(list, size) |
+                    std::views::filter([&me](const Object& obj) {
+                        return obj.AttackableFor(me) && obj.InRangeOf(me);
+                    });
+    auto num = std::ranges::distance(filtered);
+    if (num == 0) return nullptr;
+    if (num == 1) return &filtered.front();
+    return diff ? &(*std::ranges::min_element(filtered, dif_cmp)) : &(*std::ranges::min_element(filtered));
 }
